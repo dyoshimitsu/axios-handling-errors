dfd24df761e8596b6da63a9f8114b6875a20126e
dyoshimitsu <contact@dyoshimitsu.com>
2024-03-20 00:52:59 +0900

    add example

--- fetchData.ts
+++ fetchData.ts
@@ -1,12 +1,25 @@
 import axios from 'axios';
 
 async function fetchData() {
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-    console.log(response.data);
-  } catch (error) {
-    console.error(`Error occurred: ${error}`);
-  }
+  axios.get('/user/12345')
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
 }
 
 fetchData();
